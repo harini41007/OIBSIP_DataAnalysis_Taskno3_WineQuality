c09d075937ce7195c800dda9a9d860d2d2caf951
Harini41027 <harinithillaikumaran@gmail.com>
2026-06-12 23:20:37 +0530

    Update WineQuality code.py

--- WineQuality code.py
+++ WineQuality code.py
@@ -76,7 +76,7 @@ plt.barh(features,importances)
 plt.title("Feature Importance")
 plt.show()
 
-# Sample  wine data for testing - cut off
+# Sample  wine data for testing 
 sample = [[7.4,0.7,0,1.9,0.076,11,34,0.9978,3.51,0.56,9.4,0.3]]
 
 prediction = rf_model.predict(sample)
